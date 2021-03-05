92631a741d6e4964b701f916e11aa5c1c6b722af
jonathan vanasco <jonathan@2xlp.com>
2021-03-05 12:02:18 -0500

    prepping for 0.1.1

--- bleach_extras/__init__.py
+++ bleach_extras/__init__.py
@@ -4,9 +4,9 @@ from pkg_resources import parse_version
 
 
 # yyyymmdd
-__releasedate__ = "20201020"
+__releasedate__ = "20210305"
 # x.y.z or x.y.z.dev0 -- semver
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 VERSION = parse_version(__version__)
 
 
